bf583a2a7229e13cf1aefc0c64262e8e706d40b4
Mohd Abrar <abrarmemon12578@gmail.com>
2026-09-19 19:58:38 +0530

    Scaffold React web app with login screen

--- web/src/screens/LoginScreen.jsx
+++ web/src/screens/LoginScreen.jsx
@@ -1,12 +1,11 @@
 export default function LoginScreen() {
-    const handleLogin = () => {
-        window.location.href = 'http://localhost:8080/oauth2/authorization/google';
-    };
-    return(
-        <div>
-            <h1>Context-Aware Assistant</h1>
-            <button> onClick={handleLogin}>Login with Google</button>
-        </div>
-
-    );
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  };
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Context-Aware Assistant</h1>
+      <button onClick={handleLogin}>Login with Google</button>
+    </div>
+  );
 }
