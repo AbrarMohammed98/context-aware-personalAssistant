295568de5187645961f96c9dec3fdb7d71c264f9
Mohd Abrar <abrarmemon12578@gmail.com>
2026-09-21 22:28:29 +0530

    Add protected route guard for dashboard

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import AuthCallback from './screens/AuthCallback';
 import DashboardScreen from './screens/DashboardScreen';
-
+import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginScreen />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
-        <Route path="/dashboard" element={<DashboardScreen />} />
+        <Route path="/dashboard" element={<ProtectedRoute><DashboardScreen /></ProtectedRoute>}/>
       </Routes>
     </BrowserRouter>
   );
