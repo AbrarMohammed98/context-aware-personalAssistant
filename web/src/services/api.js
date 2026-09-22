8a0b77d3dcbc38c02f59ddd41186c46ea2c85472
Mohd Abrar <abrarmemon12578@gmail.com>
2026-09-22 08:24:08 +0530

    Add response interceptor to handle expired/invalid tokens

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -7,4 +7,15 @@ api.interceptors.request.use((config) => {
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+      localStorage.removeItem('jwt_token');
+      window.location.href = '/';
+    }
+    return Promise.reject(error);
+  }
+);
 export default api;
